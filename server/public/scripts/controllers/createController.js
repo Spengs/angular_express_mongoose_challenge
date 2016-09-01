255583f4ec0c0b1bcdb768ae82d1c20e025e4f9b
Spengs <Jotto465@gmail.com>
2016-09-01 12:30:05 -0500

    so close

--- server/public/scripts/controllers/createController.js
+++ server/public/scripts/controllers/createController.js
@@ -2,6 +2,17 @@ myApp.controller('CreateController', ['$scope', '$http', function ($scope, $http
   console.log('CreateController running!');
   // $scope.heroes = [];
   $scope.newHero = {};
+  $scope.power = {};
+  $scope.powers = [
+    {type:"Invisibility", display: "Invisibility"},
+    {type:"Flight", display: "Flight"},
+    {type:"Super Speed", display: "Super Speed"},
+    {type:"Heat Vision", display: "Heat Vision"},
+    {type:"Super Strength", display: "Super Strength"},
+    {type:"Accelerated Healing", display: "Accelerated Healing"},
+    {type:"Power Blast", display: "Power Blast"},
+    {type:"Animal Affinity", display: "Animal Affinity"},
+  ];
 
   $scope.submitHero = function () {
       var data = $scope.newHero;
